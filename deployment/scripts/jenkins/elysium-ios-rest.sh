a09ab899b6b1bc3d33da0be194b945f55ceb14c0
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-04-18 11:40:22 +0100

    adding more jenkins tasks

---
+++ deployment/scripts/jenkins/elysium-ios-rest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+#set -o verbose
+
+# Fetch code
+git clone git@gitlab.io.thehut.local:elysium/elysium-e2e-mobile.git
+cd elysium-e2e-mobile
+
+# Executing tests
+npm --version
+npm install
+npm run e2e-ios-main
+wait
+npm run e2e-get-result
