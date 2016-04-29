f599f2c4e87c23b74d89fc881cf8be823fb61027
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-04-29 11:13:00 +0100

    adding main run sh for jenkins

---
+++ deployment/scripts/jenkins/elysium-e2e-mobile-topsites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+#set -o verbose
+
+# Fetch code
+git clone git@gitlab.io.thehut.local:elysium/elysium-e2e-mobile.git
+cd elysium-e2e-mobile
+
+# Executing tests
+npm --version
+npm install
+mkdir reports
+npm run e2e-ios-main
+npm run e2e-android-main
+wait
+npm run e2e-get-result
